9d14aa942dfa88dc7c4c9e841a2089f4e6f90e6b
JadenLund <lundjaden@gmail.com>
2023-04-19 13:59:41 -0500

    mvp of cat

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,8 @@ import catCard from "./catCard"
 
 export default function layout() {
     return (
-        <><div class="d-flex flex-column justify-content-center w-100 h-100"></div>
+        <>
+            <div className="background"></div>
             <catCard />
         </>
 
