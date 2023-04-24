ec871b8a4ee3b85fd33ec901e04f5a8054d9a40f
JadenLund <lundjaden@gmail.com>
2023-04-24 16:48:24 -0500

    changing from layout to pagesetup for cleaner grid designing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { ReactDOM } from "react";
-import CatCard from "./components/catCard";
-import Layout from "./components/layout";
+// import { ReactDOM } from "react";
 
+import PageSetup from "./components/pageSetup";
 export default function App() {
-  // const [image, setImage] = React.useState({ image: "" });
-
-  // React.useEffect(() => {
-  //   fetch('https://api.thecatapi.com/v1/images/search?api_key=live_cryzeMviH86EMISpx1pIcPxIE0uVDaKMbZ2gFSeCEipxAwEztUwW38LZ75mtwl8h')
-  //     .then(resp => resp.json())
-  //     .then(data => setImage(data[0].url))
-  // }, [])
 
   return (
-    <Layout />
+    <PageSetup />
   );
 };
 
